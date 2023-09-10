e1b15872a2f39029a14abda537d4bfedefcb2213
khris-xp <khrisbharmmano@gmail.com>
2023-09-10 10:26:55 +0700

    feat: install dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
+import fileUpload from 'express-fileupload';
+import mongoose, { Mongoose } from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}));
+
 const port = process.env.PORT;
+const URL = process.env.MONGODB_URI;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.json({ message: 'Hello World, from X CLone API.' });
 });
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+mongoose.connect(URL as string).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
