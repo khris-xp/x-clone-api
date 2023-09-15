19886b6d3c87cbc5bc1cea220c3d2245dfef589c
khris-xp <khrisbharmmano@gmail.com>
2023-09-16 00:17:02 +0700

    feat: add routes to get tweet by user and retweet

--- routes/tweet.route.ts
+++ routes/tweet.route.ts
@@ -5,5 +5,8 @@ import authUser from "../middlewares/auth.middleware";
 tweetRouter.post('/', authUser, tweetController.createTweet);
 tweetRouter.get('/', authUser, tweetController.getTweets);
 tweetRouter.get('/:id', authUser, tweetController.getTweet);
+tweetRouter.get('/user/:id', authUser, tweetController.getTweetByUser);
+tweetRouter.post('/:id/like', authUser, tweetController.likeTweet);
+tweetRouter.post('/:id/retweet', authUser, tweetController.reTweet);
 
 export default tweetRouter;
